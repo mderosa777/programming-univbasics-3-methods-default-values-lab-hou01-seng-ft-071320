6402123a3893c9971d882d78ea1ae154f1b29c2a
megan <meggsnbacon777@gmail.com>
2020-06-30 01:47:49 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,17 @@
+  $greeting ="what a nutrious meal"
+
+def meal_choice(veg1,veg2,protien = "tofu")
+ "A plate of #{protien} with #{veg1} and #{veg2}." 
+end
+
+ meal_choice('carrots','string beans','apples')
+meal_choice('lentil','carrots')
+
+
+
+
+
+
+
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
